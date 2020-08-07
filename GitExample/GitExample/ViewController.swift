935e8c16f41135714d8334a403001951ac44e9a4
Hoang Tuan Anh <anhht@hybrid-technologies.vn>
2020-08-07 16:01:53 +0700

    add title

--- GitExample/GitExample/ViewController.swift
+++ GitExample/GitExample/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbTitle: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lbTitle.text = "Hello World"
     }
 
 
